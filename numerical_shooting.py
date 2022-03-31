335fd5e4421f873139d73011753c5b46abb26e5f
benlecoz <go19111@bristol.ac.uk>
2022-03-31 18:23:05 +0100

    Added error graph for shooting code on general ODEs

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from ODE_solver import solve_ode, SO_plot
 from scipy.optimize import fsolve
+from math import sqrt, cos, sin
 
 
 def SO_f2(X, t, args):
@@ -32,15 +33,31 @@ def Hopf_bif(U, t, args):
     return dudt
 
 
-def shooting(f, u0, args):
+def Hopf_true_sol(t, args):
+
+    beta = args[0]
+    # phase_con = args[1]
+    u1 = np.zeros(len(t))
+    u2 = np.zeros(len(t))
+
+    for i in range(len(t)):
+        # u1[i] = sqrt(beta) * cos(t[i] + phase_con)
+        # u2[i] = sqrt(beta) * sin(t[i] + phase_con)
+        u1[i] = sqrt(beta) * cos(t[i])
+        u2[i] = sqrt(beta) * sin(t[i])
+
+    return u1, u2
+
+
+def shooting(f, u0, *args):
 
     def conds(u0):
 
         x0, t = u0[:-1], u0[-1:]
 
-        sol, sol_time = solve_ode(f, x0, 0, t, 'RK4', 0.01, args)
+        sol, sol_time = solve_ode(f, x0, 0, t, 'RK4', 0.01, *args)
 
-        phase_con = f(x0, t, args)[0]
+        phase_con = f(x0, t, *args)[0]
         period_con1 = x0[0] - sol[-1, 0]
         period_con2 = x0[1] - sol[-1, 1]
         period_con = np.array([period_con1, period_con2])
@@ -52,39 +69,57 @@ def shooting(f, u0, args):
     return real_sol
 
 
-def shooting_cycle(f, solution, args):
+def shooting_cycle(f, f_sol, solution, error, *args):
 
     x0, t = solution[:-1], solution[-1]
 
-    SO_plot(f, x0, 0, t, args)
+    X, T = SO_plot(f, x0, 0, t, *args)
+
+    if error == 'yes':
+
+        u1, u2 = f_sol(T, *args)
+
+        error1 = np.zeros(len(T))
+        error2 = np.zeros(len(T))
+
+        for i in range(len(T)):
+            error1[i] = abs(u1[i] - X[i, 0])
+            error2[i] = abs(u2[i] - X[i, 1])
+
+        plt.plot(T, error1, label = 'S1 error')
+        plt.plot(T, error2, label = 'S2 error')
+        plt.legend()
+        plt.show()
 
 
-def shooting_orbit(f, solution, args):
+def shooting_orbit(f, solution, *args):
 
     x0, t = solution[:-1], solution[-1]
 
-    sol, sol_time = solve_ode(f, x0, 0, t, 'RK4', 0.01, args)
+    sol, sol_time = solve_ode(f, x0, 0, t, 'RK4', 0.01, *args)
 
     plt.plot(sol[:, 0], sol[:, 1])
     plt.show()
 
 
-args = [1, 0.16, 0.1]
-shooting_solution = shooting(SO_f2, [0.2, 0.2, 21], args)
-
-print(shooting_solution)
-
-shooting_cycle(SO_f2, shooting_solution, args)
-shooting_orbit(SO_f2, shooting_solution, args)
-
-
-# args = [1, -1]
-# shooting_solution = shooting(Hopf_bif, [1, 1, 8], args)
+# args = [1, 0.16, 0.1]
+# shooting_solution = shooting(SO_f2, [0.2, 0.2, 21], args)
 #
 # print(shooting_solution)
 #
-# shooting_cycle(Hopf_bif, shooting_solution, args)
+# shooting_cycle(SO_f2, shooting_solution, args)
+# shooting_orbit(SO_f2, shooting_solution, args)
+
+
+args = [1, -1]
+shooting_solution = shooting(Hopf_bif, [1.2, 1.2, 8], args)
+
+print(shooting_solution)
+
+shooting_cycle(Hopf_bif, Hopf_true_sol, shooting_solution, 'yes', args)
+# shooting_true_cycle(Hopf_bif, shooting_solution, args)
 # shooting_orbit(Hopf_bif, shooting_solution, args)
+
 
 
 
